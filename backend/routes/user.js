4860ba9643a4b0aeb00c1fdb27936193140f7636
Antonio00000000 <antonio.todaro04@gmail.com>
2026-06-02 13:15:15 +0200

    correzione metodo di accesso e di registrazione

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,10 @@
+
 const mongoose = require('mongoose');
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
+
 mongoose.connect('mongodb://localhost:27017/utenti')
 .then(() => console.log('Connesso al database utenti'))
 .catch(err => console.error('Errore di connessione al database utenti:', err));
@@ -9,37 +13,52 @@ const utenteSchema = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
+        required: true
     },
-    password: String,
-    dipartimento: String
+    password: {
+        type: String,
+        required: true
+    },
+    dipartimento: {
+        type: String,
+        required: true
+    }
 });
+
+const Utente = mongoose.model('Utente', utenteSchema);
 
 //crea un nuovo utente
 const nuovoUtente = async (username, password, dipartimento) => {
     //mi salvo la password criptata
     let psw = await bcrypt.hash(password, 10);
-    const utente = new Utente({ username, psw, dipartimento });
-    return await utente.create();
+    
+    //mappature per creare un nuovo utente con i dati inseriti e salvarlo nel database
+    const utente = new Utente({ username,password: psw, dipartimento });
+    return await utente.save();
 };
 
 //funzione per controllare se le credenziali sono corrette al login
 const controllo = async (username, password, dipartimento) => {
-    try {
+try {
         const user = await Utente.findOne({ username });
-        //confronto la password inserita con quella salvata nel database hashata
-        if (bcrypt.compare(password, user.password)) {
+        
+        // controllo se l'utente esiste effettivamente nel database
+        if (!user) {
+            throw new Error('Credenziali non valide o devi prima registrarti');
+        }
+
+        // mi compara la password inserita con quella salvata nel database
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if (isMatch) {
             return true;
         } else {
             throw new Error('Credenziali non valide o devi prima registrarti');
         }
-    }catch (err) {
+    } catch (err) {
         console.error('Errore durante il controllo delle credenziali:', err);
         throw err;  
     }
 };
-
-
-
-const Utente = mongoose.model('Utente', utenteSchema);
 
 module.exports = router;
